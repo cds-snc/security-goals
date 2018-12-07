fa51a322675c787d547bdca0e3a44e5fe24c7044
Tim Arney <tim@line37.com>
2018-12-07 09:19:31 -0500

    clean compliance target + tests

--- compliance-api/src/__tests__/createCompliance.test.js
+++ compliance-api/src/__tests__/createCompliance.test.js
@@ -1,7 +1,6 @@
 /* global fetch */
 global.fetch = require('jest-fetch-mock')
 const fs = require('fs')
-const { getChecks } = require('../getChecks.js')
 const { fetchYaml } = require('../fetchYaml.js')
 const { createCompliance } = require('../createCompliance.js')
 
@@ -28,30 +27,12 @@ let checks, certification, definitions
 describe('createCompliance', () => {
   beforeAll(async () => {
     // real fixture path
-    checks = await getChecks('src/__tests__/testData/checks')
+    checks = []
     // fake but valid url, response is just from the mock
     // first call gets the first mock with the certification
     certification = await fetchYaml('https://example.com/foo.yaml')
     // second call gets second mock with the full definitions
     definitions = await fetchYaml('https://example.com/ITSG-33a.yaml')
-  })
-
-  it('combines the certification and checks', async () => {
-    let compliancePosture = await createCompliance({
-      definitions,
-      checks,
-      certification,
-    })
-
-    let certificationKeys = Object.keys(certification.standards['ITSG-33a'])
-    let checkKeys = checks.reduce((arr, chk) => [...arr, ...chk.satisfies], [])
-
-    let complianceKeysWithNoDuplicates = Array.from(
-      new Set([...certificationKeys, ...checkKeys]),
-    )
-
-    let keys = Object.keys(compliancePosture)
-    expect(keys).toEqual(complianceKeysWithNoDuplicates)
   })
 
   describe('without checks', () => {
@@ -64,18 +45,6 @@ describe('createCompliance', () => {
 
       let keys = Object.keys(compliancePosture)
       expect(keys).toHaveLength(3)
-    })
-  })
-  describe('with checks', () => {
-    it('creates an object with controls from the certification and the checks', async () => {
-      let compliancePosture = await createCompliance({
-        definitions,
-        checks,
-        certification,
-      })
-
-      let keys = Object.keys(compliancePosture)
-      expect(keys).toHaveLength(13)
     })
   })
 })
