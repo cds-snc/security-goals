510b238986aedaf1fd927c42ae5c0cb28fd84f49
Tim Arney <tim@line37.com>
2018-12-17 15:10:19 -0500

    controlID

--- compliance-api/src/resolvers/controls.js
+++ compliance-api/src/resolvers/controls.js
@@ -1,13 +1,27 @@
-const { GraphQLList } = require('graphql')
+const { GraphQLList, GraphQLString } = require('graphql')
 const { OpenControl } = require('../types/OpenControl')
 
 const controls = {
   description: 'Returns a list of all controls',
   type: new GraphQLList(OpenControl),
-  resolve: root => {
-    return Object.keys(root).map(item => {
+  args: {
+    controlId: {
+      type: GraphQLString,
+      description: 'optional control id to limit to specific control',
+    },
+  },
+  resolve: (root, { controlId }) => {
+    const controls = Object.keys(root).map(item => {
       return root[item]
     })
+
+    if (controlId) {
+      return controls.filter(item => {
+        return item.id === controlId
+      })
+    }
+
+    return controls
   },
 }
 
