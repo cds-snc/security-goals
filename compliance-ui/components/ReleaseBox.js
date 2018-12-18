7da28aafb8b1228a2c1ad7800e869da813d806dc
nmakuch <makuch.nick@gmail.com>
2018-12-18 18:51:03 -0500

    beggining of mediaQueries (restructure)

--- compliance-ui/components/ReleaseBox.js
+++ compliance-ui/components/ReleaseBox.js
@@ -20,21 +20,19 @@ const releaseBoxPassing = css`
     font-weight: 700;
   }
 
-  div:first-of-type {
+  div[name="inner-container"] {
     display: flex;
     font-size: ${theme.font.lg};
-    color: ${theme.colour.blackLight};
+    color: ${theme.colour.black};
     justify-content: space-between;
     margin-bottom: ${theme.spacing.xs};
   }
 
-  div:first-of-type div:first-of-type span:first-of-type {
-    font-weight: 700;
-  }
-
-  div:first-of-type div:last-of-type span {
-    font-weight: 700;
-  }
+  ${mediaQuery.lg(css`
+    div[name="inner-container"] {
+      display: block;
+    }
+  `)};
 `;
 
 const releaseBoxFailing = css`
@@ -51,15 +49,16 @@ const passingText = css`
   padding: ${theme.spacing.xs};
   font-size: ${theme.font.sm};
   border-radius: 5px;
+
+  ${mediaQuery.lg(css`
+    margin: 0 ${theme.spacing.sm} 0 0;
+  `)};
 `;
 
 const failingText = css`
-  color: ${theme.colour.white};
+${passingText}
   background: ${theme.colour.redDark};
-  margin-left: ${theme.spacing.sm};
-  padding: ${theme.spacing.xs};
-  font-size: ${theme.font.sm};
-  border-radius: 5px;
+
 `;
 
 const releaseFocusPassing = css`
@@ -80,6 +79,30 @@ const releaseFocusFailing = css`
   }
 `;
 
+const releaseTitle = css`
+  width: 70%;
+
+  h3[name="releasebox-title"] {
+    font-size: ${theme.font.lg};
+    margin: 0 0 ${theme.spacing.sm} 0;
+  }
+
+  ${mediaQuery.lg(css`
+    width: 100%;
+  `)};
+`;
+const releaseBadges = css`
+  width: 30%;
+  font-weight: 700;
+  text-align: right;
+
+  ${mediaQuery.lg(css`
+    text-align: left;
+    width: 100%;
+    margin-top: ${theme.spacing.lg};
+  `)};
+`;
+
 const ReleaseBox = ({ release, timestamp, passed, passing, total, link }) => {
   return (
     <li>
@@ -97,11 +120,12 @@ const ReleaseBox = ({ release, timestamp, passed, passing, total, link }) => {
               passed === "true" ? releaseBoxPassing : releaseBoxFailing
             }
           >
-            <div>
-              <div>
-                <span name="releasebox-title">Release: #{release}</span>{" "}
+            <div name="inner-container">
+              <div className={releaseTitle}>
+                <h3 name="releasebox-title">Release: #{release}</h3>{" "}
+                <p name="releasebox-timestamp">{timestamp.toGMTString()}</p>
               </div>
-              <div>
+              <div className={releaseBadges}>
                 <span
                   name="releasebox-passing"
                   className={passed === "true" ? passingText : failingText}
@@ -116,7 +140,6 @@ const ReleaseBox = ({ release, timestamp, passed, passing, total, link }) => {
                 </span>
               </div>
             </div>
-            <p name="releasebox-timestamp">{timestamp.toGMTString()}</p>
           </div>
         </a>
       </Link>
