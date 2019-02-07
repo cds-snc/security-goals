9e8dcca68ea8796527526c84ad9d82b6c21d9bd2
nmakuch <makuch.nick@gmail.com>
2019-02-07 13:56:24 -0500

    KEYBOARD A11Y WORKING ON SINGLERELEASE :DDD

--- compliance-ui/components/controlbox/WithLink.js
+++ compliance-ui/components/controlbox/WithLink.js
@@ -1,11 +1,16 @@
 import Link from "next/link";
 
-export const WithLink = ({ children, id, link }) => {
+export const WithLink = ({ children, id, link, keyDown }) => {
   const url = `/controls/${id}`;
   const label = `Control: ${id}`;
   return link ? (
     <Link as={url} href={`/details?control=${id}`}>
-      <a data-testid="control-box-link" aria-label={label}>
+      <a
+        name="control-link"
+        onKeyDown={keyDown}
+        data-testid="control-box-link"
+        aria-label={label}
+      >
         {children}
       </a>
     </Link>
