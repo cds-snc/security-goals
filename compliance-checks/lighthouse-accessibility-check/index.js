a596e415915d828763bd6b37f187f3a2a1457c21
Tim Arney <timarney@users.noreply.github.com>
2019-02-11 08:59:36 -0500

    Add lighthouse check (#47)
    * add lighthouse check
    
    * fix .gitingore
    
    * Delete .env
    
    * update .gitignore
    
    * update report to pull from env vars
    
    * update docker
    
    * update .gitignore
    
    * update docker
    
    * Changed around docker container

---
+++ compliance-checks/lighthouse-accessibility-check/index.js
@@ -0,0 +1,31 @@
+const { scanURL, writeCheckFile, checkFileContent } = require("./lib");
+
+(async () => {
+  try {
+    /*
+    // for testing
+    const data = {
+      categories: {
+        pwa: {
+          score: 0.3
+        },
+        speed: {
+          score: 0.5
+        }
+      }
+    };
+    */
+
+    const data = await scanURL();
+
+    if (!data || !data.categories) {
+      throw new Error("Unable to retreive lighthouse data");
+    }
+
+    const categories = data.categories;
+    const path = process.env.OUT_PATH ? process.env.OUT_PATH : "";
+    writeCheckFile(path, checkFileContent(categories));
+  } catch (e) {
+    console.log(e.message);
+  }
+})();
---
+++ compliance-checks/lighthouse-accessibility-check/lib.js
@@ -0,0 +1,115 @@
+const path = require("path");
+const fs = require("fs");
+const lighthouse = require("lighthouse");
+const chromeLauncher = require("chrome-launcher");
+const BrowserFetcher = require("puppeteer/lib/BrowserFetcher");
+
+/* IMPORTANT ensure the correct node modules path here */
+const browserFetcher = new BrowserFetcher(
+  path.join(__dirname, "node_modules/puppeteer/")
+);
+
+const packageJson = require("puppeteer/package.json");
+const revision = packageJson.puppeteer.chromium_revision;
+const revisionInfo = browserFetcher.revisionInfo(revision);
+
+let opts = {
+  lighthouseFlags: {
+    output: "json",
+    disableDeviceEmulation: false,
+    port: "",
+    output: "json",
+    saveAssets: false
+  },
+  chromeFlags: ["--headless", "--no-sandbox", "--disable-gpu"]
+};
+
+const writeCheckFile = (path = "", content = "") => {
+  fs.writeFile(`${path}report.json`, content, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("The file was saved!");
+  });
+};
+
+const getValues = (data = {}) => {
+  let report = [];
+  Object.keys(data).forEach(function(key) {
+    let score = data[key].score;
+    score = Math.round(score * 100);
+    report.push(`${key} ${score}`);
+  });
+
+  return report.join(",");
+};
+
+const checkReport = {
+  origin: "",
+  satisfies: [],
+  component: "Source code",
+  references: "",
+  timestamp: Date.now(),
+  passed: true,
+  description: ""
+};
+
+const populateCheckContent = description => {
+  const ORIGIN = process.env.ORIGIN ? process.env.ORIGIN : "Missing origin";
+
+  const SATISFIES = process.env.SATISFIES
+    ? process.env.SATISFIES.split(",")
+    : "";
+
+  const COMPONENT = process.env.COMPONENT
+    ? process.env.COMPONENT
+    : "Missing component";
+
+  const URL = process.env.URL ? process.env.URL : "Missing Url";
+
+  return {
+    ...checkReport,
+    ...{
+      description: description,
+      origin: ORIGIN,
+      satisfies: SATISFIES,
+      component: COMPONENT,
+      references: URL
+    }
+  };
+};
+
+const checkFileContent = data => {
+  return JSON.stringify(populateCheckContent(getValues(data)));
+};
+
+const scanURL = async (url = process.env.URL) => {
+  console.log(`Launching chromeLauncher... ${url}`);
+  console.log(`chromePath: ${revisionInfo.executablePath}`);
+
+  const chrome = await chromeLauncher.launch({
+    chromeFlags: opts.chromeFlags,
+    chromePath: revisionInfo.executablePath
+  });
+
+  opts.lighthouseFlags.port = chrome.port;
+
+  console.log(`Launching lighthouse on port:${chrome.port}`);
+
+  const res = await lighthouse(url, opts.lighthouseFlags);
+  await chrome.kill();
+
+  if (res && res.report) {
+    return JSON.parse(res.report);
+  } else {
+    throw new Error("failed to pull lighthouse report");
+  }
+};
+
+module.exports.getValues = getValues;
+module.exports.populateCheckContent = populateCheckContent;
+module.exports.checkFileContent = checkFileContent;
+module.exports.checkReport = checkReport;
+module.exports.scanURL = scanURL;
+module.exports.writeCheckFile = writeCheckFile;
