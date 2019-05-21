cbbbf345f9e433d0dd09a70cf150911891cde5f3
Tim Arney <tim@line37.com>
2019-05-21 10:39:18 -0400

    add parse warning

--- api/src/db/readFile.ts
+++ api/src/db/readFile.ts
@@ -1,13 +1,22 @@
 const { promises: fs } = require('fs')
 const { basename } = require('path')
 
+import { File } from '../interfaces/File'
+
 const getFileName = (filepath: string) => {
   return basename(filepath, '.json')
 }
 
 const readFile = async (file: string): Promise<string> => {
   const content = await fs.readFile(file, { encoding: 'utf-8' })
-  const obj = JSON.parse(content)
+  let obj: File = {}
+
+  try {
+    obj = JSON.parse(content)
+  } catch (e) {
+    console.warn(`🚨error parsing ${file}`)
+  }
+
   // add filename to content for reference
   obj.fileRef = getFileName(file)
   return JSON.stringify(obj)
