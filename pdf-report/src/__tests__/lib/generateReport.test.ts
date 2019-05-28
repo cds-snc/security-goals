428000a73cfbaba1aa4c4a7b7f6845bb9deca6bc
Max <max@neuvians.net>
2019-05-28 14:33:01 -0400

    Added prettier to all projects

--- pdf-report/src/__tests__/lib/generateReport.test.ts
+++ pdf-report/src/__tests__/lib/generateReport.test.ts
@@ -2,33 +2,35 @@ import nock from "nock";
 import { generateReport } from "../../lib/generateReport";
 
 const control = {
-	"AC-1": {
-		description: "DESC",
-		name: "Access Control Policy And Procedures"
-	}
+  "AC-1": {
+    description: "DESC",
+    name: "Access Control Policy And Procedures"
+  }
 };
 
 const release = {
-	release: "2011801ed6bc445e530290ebe9d5774cd542f2f7-1558915258476",
-	timestamp: "1558915263402",
-	passed: "false",
-	passing: "13",
-	total: "14",
-	controls: [
-		{
-			control: "AC-1",
-			verifications: [
-				{
-					origin: "gcr.io/security-goals/checks/url-exists",
-					timestamp: "2019-05-27T00:01:00Z",
-					passed: "true",
-					description: "The application uses an ESLint file to do static code analysis.",
-					component: "Source code",
-					references: "https://github.com/cds-snc/cra-alpha/blob/master/.eslintrc.json"
-				}
-			]
-		}
-	]
+  controls: [
+    {
+      control: "AC-1",
+      verifications: [
+        {
+          origin: "gcr.io/security-goals/checks/url-exists",
+          timestamp: "2019-05-27T00:01:00Z",
+          passed: "true",
+          description:
+            "The application uses an ESLint file to do static code analysis.",
+          component: "Source code",
+          references:
+            "https://github.com/cds-snc/cra-alpha/blob/master/.eslintrc.json"
+        }
+      ]
+    }
+  ],
+  passed: "false",
+  passing: "13",
+  release: "2011801ed6bc445e530290ebe9d5774cd542f2f7-1558915258476",
+  timestamp: "1558915263402",
+  total: "14"
 };
 
 test("generateReport creates a PDF report", () => {
