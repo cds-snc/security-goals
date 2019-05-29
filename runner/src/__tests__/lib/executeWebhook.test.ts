20326740d39e4b9289c24f13a7332ac4630bbd95
Max <max@neuvians.net>
2019-05-29 13:22:53 -0400

    Fix test description

--- runner/src/__tests__/lib/executeWebhook.test.ts
+++ runner/src/__tests__/lib/executeWebhook.test.ts
@@ -12,7 +12,7 @@ test("does not execute the URL ", async () => {
   expect(scope.pendingMocks().length).toEqual(1);
 });
 
-test("returns a date string with the SHA of master if a GitHub repo is defined", async () => {
+test("executes the URL callback", async () => {
   process.env.CALLBACK_WEBHOOK = "https://foo/bar";
   await executeWebhook();
   expect(scope.pendingMocks().length).toEqual(0);
