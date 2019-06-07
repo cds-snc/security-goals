599330ea7326e7b2b08ecc678fc9a6e42a19d791
Max <max@neuvians.net>
2019-06-07 10:23:55 -0400

    Runner error logging

--- runner/src/lib/runJobs.ts
+++ runner/src/lib/runJobs.ts
@@ -16,53 +16,61 @@ async function asyncForEach(
 }
 
 export const runJobs = async (): Promise<void> => {
-  if (RunLock.locked) {
+
+  try {
+
+    if (RunLock.locked) {
+      return;
+    }
+
+    RunLock.locked = true;
+
+    const kc = new k8s.KubeConfig();
+
+    if (process.env.NODE_ENV === "production") {
+      kc.loadFromCluster();
+    } else {
+      kc.loadFromDefault();
+    }
+
+    const jobsApi = kc.makeApiClient(k8s.Batch_v1Api);
+    const namespace = process.env.JOBS_NAMESPACE || "security-goals";
+    // Generate release ID
+    const releaseId = await generateReleaseId();
+    const res = await jobsApi.listNamespacedJob(namespace);
+
+    if (!res.body.items) {
+      RunLock.locked = false;
+      return;
+    }
+
+    const items = uniqueArray(res.body.items);
+
+    await asyncForEach(items, async (item) => {
+      const name: string = item.metadata.name;
+      console.log("Restarting", name);
+
+      const options: V1DeleteOptions = {
+        apiVersion: "batch/v1",
+        dryRun: null,
+        gracePeriodSeconds: null,
+        kind: "DeleteOptions",
+        orphanDependents: null,
+        preconditions: null,
+        propagationPolicy: "Foreground"
+      };
+      await jobsApi.deleteNamespacedJob(name, namespace, "false", options);
+      const body = await modifyJob(item, releaseId);
+      console.log("starting", body.metadata.name);
+      await jobsApi.createNamespacedJob(namespace, body);
+    });
+
+    await executeWebhook();
+    RunLock.locked = false;
     return;
-  }
-
-  RunLock.locked = true;
-
-  const kc = new k8s.KubeConfig();
-
-  if (process.env.NODE_ENV === "production") {
-    kc.loadFromCluster();
-  } else {
-    kc.loadFromDefault();
-  }
-
-  const jobsApi = kc.makeApiClient(k8s.Batch_v1Api);
-  const namespace = process.env.JOBS_NAMESPACE || "security-goals";
-  // Generate release ID
-  const releaseId = await generateReleaseId();
-  const res = await jobsApi.listNamespacedJob(namespace);
-
-  if (!res.body.items) {
+  } catch (e) {
+    console.log("Error", e);
     RunLock.locked = false;
     return;
   }
-
-  const items = uniqueArray(res.body.items);
-
-  await asyncForEach(items, async (item) => {
-    const name: string = item.metadata.name;
-    console.log("Restarting", name);
-
-    const options: V1DeleteOptions = {
-      apiVersion: "batch/v1",
-      dryRun: null,
-      gracePeriodSeconds: null,
-      kind: "DeleteOptions",
-      orphanDependents: null,
-      preconditions: null,
-      propagationPolicy: "Foreground"
-    };
-    await jobsApi.deleteNamespacedJob(name, namespace, "false", options);
-    const body = await modifyJob(item, releaseId);
-    console.log("starting", body.metadata.name);
-    await jobsApi.createNamespacedJob(namespace, body);
-  });
-
-  await executeWebhook();
-  RunLock.locked = false;
-  return;
 };
