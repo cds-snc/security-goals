8b936c28b692e7a214e3ed004eee3be17e0d14cf
Max <max@neuvians.net>
2019-06-12 10:05:20 -0400

    Added Emailer to notifier

---
+++ notifier/src/lib/sendEmail.ts
@@ -0,0 +1,16 @@
+import FormData from "form-data";
+const fetch = require("node-fetch");
+
+export const sendEmail = async (release: any) => {
+  const token = process.env.API_TOKEN;
+  const form = new FormData();
+
+  form.append("email", process.env.EMAIL_RECIPIENTS);
+  form.append("message", "Hello how are you?");
+  const resp = await fetch(process.env.PDF_REPORT_URL);
+  const data = await resp.buffer();
+  form.append("file", data, { filename : `security-goals-report-${Date.now()}.pdf`, contentType: "application/pdf"});
+
+  const endpoint = process.env.EMAIL_ENDPOINT;
+  await fetch(endpoint, { method: "POST", body: form, headers: {Authorization: `Bearer ${token}`}});
+};
