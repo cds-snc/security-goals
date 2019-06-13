a3295a54f84d1ed01fdd011db22e00d57476df96
Tim Arney <tim@line37.com>
2019-06-13 11:20:52 -0400

    link to web report

--- performance-index/src/components/Header.js
+++ performance-index/src/components/Header.js
@@ -4,7 +4,7 @@ import { theme, mediaQuery } from "./styles";
 import { Logo } from "./Logo";
 import { I18N } from "./I18N";
 import LanguageToggleButton from "./LanguageToggle";
-import { runtimeConfig } from '../config';
+import { runtimeConfig } from "../config";
 
 const bar = css`
   background: ${theme.colour.blackLight};
@@ -72,18 +72,61 @@ const logo = css`
   `)};
 `;
 
+const actionsInner = css`
+  display: flex;
+  width: 100%;
+  justify-content: flex-start;
+  @media screen and (min-width: 900px) {
+    max-width: 85%;
+    justify-content: space-between;
+  }
+`;
+
+const btnHolder = css`
+  display: flex;
+`;
+
+const btn = css`
+  display: inline-block;
+  margin: 0 1em;
+  color: #fff;
+  margin: 1.5rem 0 0 5rem;
+`;
+
+const web_report = runtimeConfig.web_report;
+
+const actions = css`
+  display: flex;
+`;
+
 const Header = () => (
   <header data-testid="header" name="header">
     <div css={bar}>
-      <a id="back2top" href="#"></a>
+      <a id="back2top" href="#" />
       <h1 data-testid="main-header-h1">
         {runtimeConfig.app_name} - <I18N t="tagline" />
       </h1>
-      <div css={langButton}>
-        <LanguageToggleButton/>
+
+      <div css={actions}>
+        <div css={actionsInner}>
+          <div>
+            {web_report && (
+              <div css={btnHolder}>
+                <a href={web_report} css={btn}>
+                  <I18N t="web-report" />
+                </a>
+              </div>
+            )}
+          </div>
+
+          <div css={langButton}>
+            <LanguageToggleButton />
+          </div>
+        </div>
       </div>
-      <Logo alt="CDS Logo" style={logo} />
     </div>
+
+    <Logo alt="CDS Logo" style={logo} />
   </header>
 );
 
--- performance-index/src/config.js
+++ performance-index/src/config.js
@@ -1,12 +1,14 @@
 export const runtimeConfig =
-  typeof window !== 'undefined'
+  typeof window !== "undefined"
     ? {
-        api_url: (window.env ? window.env.api_url : ""),
-        app_name: (window.env ? window.env.app_name : ""),
-        relative_path: (window.env ? window.env.relative_path : "")
+        api_url: window.env ? window.env.api_url : "",
+        app_name: window.env ? window.env.app_name : "",
+        relative_path: window.env ? window.env.relative_path : "",
+        web_report: window.env ? window.env.web_report : ""
       }
     : {
         api_url: process.env.API_URL || "",
         app_name: process.env.APP_NAME || "",
-        relative_path: process.env.RELATIVE_PATH || ""
+        relative_path: process.env.RELATIVE_PATH || "",
+        web_report: process.env.WEB_REPORT || ""
       };
